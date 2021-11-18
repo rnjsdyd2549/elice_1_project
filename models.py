59f1ab3f103f6316d0e9bb1b6c3ec7d3f559fadd
kwon-yongchan <rnjsdyd2549@naver.com>
2021-11-18 22:12:46 +0900

    테이블 생성하려고 노력중 ..

--- models.py
+++ models.py
@@ -4,11 +4,13 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = 'user'
+
+    __tablename__ = 'Lib_user'
     id = db.Column(db.Integer,  primary_key=True, nullable=False, autoincrement=True)
-    user_id = db.Column(db.String(100), nullable=False, unique=True)
-    user_pw = db.Column(db.String(100), nullable=False)
-    
+    name = db.Column(db.string(10), nullable=False)
+    user_id = db.Column(db.String(20), nullable=False, unique=True)
+    user_pw = db.Column(db.String(30), nullable=False)
+
     def __init__(self,user_id,user_pw):
         self.user_id = user_id
         self.user_pw = user_pw

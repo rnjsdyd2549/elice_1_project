a9dfa2bd13d5e8706fb406e9dad4b9519bf8e9c1
kwon-yongchan <rnjsdyd2549@naver.com>
2021-11-18 22:13:03 +0900

    회원가입과 메인페이지에 책 정보나오게 수정중

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, request
+import sqlite3 as sql
 bp = Blueprint('bp', __name__)
 
 @bp.route('/')
@@ -9,6 +10,22 @@ def home_login():
 def register():
     return render_template('join.html')
 
-@bp.route('/main', methods=["GET"])
+@bp.route('/join', method=['POST'])
+def adduser():
+    name =request.form['name']
+    userId = request.form['userId']
+    userPw = request.form['userPw']
+    userPw2 = request.form['userPw2']
+
+    with sql.connect('tb_book_info.db') as con:
+        cur =con.cursor()
+        cur.execute('INSERT INTO Lib_user(id, name, ')
+    
+@bp.route('/main')
 def main():
+    # with sql.connect('tb_book_info.db') as con:
+    #     cur =con.cursor()
+    #     cur.execute('SELECT * FROM tb_book_info')
+    #     result = cur.fetchall()
+    #     print(result)
     return render_template('main.html')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+with sql.connect('tb_book_info.db') as con:
+    #     cur =con.cursor()
+    #     cur.execute('SELECT * FROM tb_book_info')
+    #     result = cur.fetchall()
+    #     print(result)

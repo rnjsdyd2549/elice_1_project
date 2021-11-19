365048046a7f5b0218315490ace29f60ab4d8d2e
kwon-yongchan <rnjsdyd2549@naver.com>
2021-11-19 23:14:11 +0900

    db 연결 ! username 문제..

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, jsonify, render_template, request
 import sqlite3 as sql
+from db_connect import db
+
+from models import Lib_User
 bp = Blueprint('bp', __name__)
 
 @bp.route('/')
@@ -12,14 +15,14 @@ def register():
 
 @bp.route('/join', methods=['POST'])
 def adduser():
-    userName =request.form['userName']
+    userName = request.form['userName']
     userId = request.form['userId']
     userPw = request.form['userPw']
-    userPw2 = request.form['userPw2']
 
-    with sql.connect('tb_book_info.db') as con:
-        cur =con.cursor()
-        cur.execute('INSERT INTO Lib_user(id, name, ')
+    user = Lib_User(userName, userId, userPw)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"result": "success"})
     
 @bp.route('/main')
 def main():
